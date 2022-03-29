f258c39512ed62a51de01d96766b71e18233c2a0
AdityaKumar <adi54321kumar@outlook.com>
2022-03-29 13:16:45 +0530

    adjusting the floating button(final commit before mid)

---
+++ app/src/main/java/com/example/timetablemanager/futuretask/addtask.kt
@@ -0,0 +1,15 @@
+package com.example.timetablemanager.futuretask
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.timetablemanager.R
+
+class addtask:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_add_task)
+
+
+    }
+}
